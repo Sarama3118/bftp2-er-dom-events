a77c1dc4cf5c4dada03d1d9c21f8f792cf9739ad
Sarama3118 <ramissara@gmail.com>
2021-11-03 09:34:38 +0100

    añadido clear button

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
     let tasks = [];
-    $('#submit').on("click", function() {
-        let newTask = $('#newTask').val();
-        if (newTask !== "") {
-        tasks.push(newTask);
-    
-        $('#taskList').empty()
-        
-tasks.forEach( t => 
-    $('#taskList').append(
-        `<div class="task">${t}</div>`
-    )
-);
 
-    }
-(`Tenemos las siguientes tareas en la lista: ${tasks}`);
-        
+    $('#submit').on("click", function () {
+        let newTask = $('#newTask').val();
+
+        if (newTask !== "") {
+
+            tasks.push(newTask);
+
+            $('#taskList').empty()
+
+            tasks.forEach(t =>
+                $('#taskList').append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+    $('.clearButton').on("click", function () { 
+        $(".task").remove();
+        tasks = [];
     })
+})
 });
-	
